c941fa86a75c4651ef71b1eb52c849f764125eea
HardBoss07 <matteo@bosshard.net>
2025-04-20 12:22:22 +0200

    added ReaderIcon.tsx to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import Image from 'next/image';
 import Header from "@/app/components/Header";
 import {GeneratorIcon} from "@/app/components/GeneratorIcon";
+import {ReaderIcon} from "@/app/components/ReaderIcon";
 
 export default function Home() {
     return (
@@ -12,21 +12,13 @@ export default function Home() {
                 <h1 className="text-3xl font-bold mb-8 text-center">Barcode Generator and Reader</h1>
 
                 <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 w-full max-w-2xl">
-                    <a href="./generate"
-                       className="bg-white shadow-md rounded-xl overflow-hidden hover:shadow-lg transition">
+                    <a href="./generate" className="bg-white shadow-md rounded-xl overflow-hidden hover:shadow-lg transition">
                         <GeneratorIcon/>
                         <div className="p-4 text-center text-lg font-semibold">Generator</div>
                     </a>
 
-                    <a href="./read"
-                       className="bg-white shadow-md rounded-xl overflow-hidden hover:shadow-lg transition">
-                        <Image
-                            src="/reader.png"
-                            alt="Reader"
-                            width={459}
-                            height={317}
-                            className="w-full h-auto object-cover"
-                        />
+                    <a href="./read" className="bg-white shadow-md rounded-xl overflow-hidden hover:shadow-lg transition">
+                        <ReaderIcon/>
                         <div className="p-4 text-center text-lg font-semibold">Reader</div>
                     </a>
                 </div>
