4e3427fa416f1697e3778bcb6f0f002018c001d1
HardBoss07 <matteo@bosshard.net>
2025-10-05 10:55:48 +0200

    updated next.config.ts for custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export"
+  output: "export",
+  basePath: "/qr",
+  assetPrefix: "/qr/",
 };
 
 export default nextConfig;
